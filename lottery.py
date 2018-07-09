510c61c2b57c10c5e90ff02786997fdef0c8d8c9
T <twisummerland@gmail.com>
2018-07-08 19:37:59 -0700

    refactoring code

--- lottery.py
+++ lottery.py
@@ -15,6 +15,23 @@
 #
 # 4938532894754 -> 49 38 53 28 9 47 54
 # 1234567 -> 1 2 3 4 5 6 7
+# README instructions:
+#
+#
+# To Run: use the command python lottery.py [string values]
+# example: python lottery.py "1234567" "12345678"
+# The program will print out 1234567 and 12345678 since they are both valid strings
+
+# Thought process: I looked at the problem as a sliding window permutation problem. Depending on the length
+# of the string I would know how many numbers were single digit numbers and how many numbers were double digit
+# numbers. For example: 1234567 has seven single digit numbers and no double digit numbers while 12345678 has six
+# single digit numbers and 1 double digit number.
+#
+# Knowing how many single and double digit numbers a string has I created a recursive solution where we iterate through
+# the possible solutions by choosing a to make a number from a single digit or a double digit and then continue to
+# recurse. To optimize my solution I do a check to make sure that the number is valid before recursing again.
+# If it doesn't 
+
 
 
 import sys
@@ -59,6 +76,7 @@ class TestLottery(unittest.TestCase):
         self.assertTrue(Lottery.is_lottery('12345678'))
         self.assertTrue(Lottery.is_lottery('123456789'))
         self.assertTrue(Lottery.is_lottery('12345678911'))
+        self.assertTrue(Lottery.is_lottery('4938532894754'))
 
 
 class Lottery:
@@ -75,7 +93,7 @@ class Lottery:
         single_digit = MAX_DIGITS - double_digit
 
         is_lottery = Lottery.create_digit(single_digit, double_digit, set(), str_nums)
-        if is_lottery == True:
+        if is_lottery is True:
             return True
         else:
             return False
@@ -83,21 +101,20 @@ class Lottery:
     @staticmethod
     def create_digit(single, double, num_set, str):
 
-        print "single: {} double: {} arr: {} str: {}".format(single, double, num_set, str)
+        # print "single: {} double: {} arr: {} str: {}".format(single, double, num_set, str)
         is_lottery = False
         if len(str) == 0 and single == 0 and double == 0:
-            print num_set
+            # print num_set
             is_lottery = True
         else:
-            if(double > 0):
-                new_set = num_set.copy()
-                if(str[0] != '0'):
+            new_set = num_set.copy()
+            if double > 0:
+                if str[0] != '0':
                     temp_num = int(str[0:2])
                     if temp_num > 0 and temp_num <= MAX_NUM and temp_num not in new_set:
                         new_set.add(temp_num)
                         is_lottery = is_lottery or Lottery.create_digit(single, double-1, new_set, str[2:])
             if single > 0:
-                new_set = num_set.copy()
                 temp_num = int(str[0])
                 if temp_num > 0 and temp_num <= MAX_NUM and temp_num not in new_set:
                     new_set.add(temp_num)
@@ -107,13 +124,12 @@ class Lottery:
 
 def main(argv):
     arr = ast.literal_eval(str(argv[1:]))
-    print arr
+    # print arr
     for val in arr:
         if Lottery.is_lottery(val):
             print val
 
 
 if __name__ == "__main__":
-    unittest.main()
-    # "569815571556" "4938532894754" "1234567" "472844278465445" "7103013934"
+    # unittest.main()
     main(sys.argv)
