bc9eb5f653c3a8e99927f3300ce0f9eb0cd58752
T <twisummerland@gmail.com>
2018-07-08 20:34:30 -0700

    writing explanation

--- lottery.py
+++ lottery.py
@@ -21,16 +21,21 @@
 # To Run: use the command python lottery.py [string values]
 # example: python lottery.py "1234567" "12345678"
 # The program will print out 1234567 and 12345678 since they are both valid strings
-
+#
 # Thought process: I looked at the problem as a sliding window permutation problem. Depending on the length
 # of the string I would know how many numbers were single digit numbers and how many numbers were double digit
 # numbers. For example: 1234567 has seven single digit numbers and no double digit numbers while 12345678 has six
 # single digit numbers and 1 double digit number.
 #
 # Knowing how many single and double digit numbers a string has I created a recursive solution where we iterate through
-# the possible solutions by choosing a to make a number from a single digit or a double digit and then continue to
+# the possible solutions by choosing to make a number from a single digit or a double digit and then continue to
 # recurse. To optimize my solution I do a check to make sure that the number is valid before recursing again.
-# If it doesn't 
+# If it isn't valid we eliminate all possible values resulting from the number. Also if we find a solution we return
+# right away.  To keep track of numbers used, I'm using a set, for fast lookup and insertion O(1).  The worst case times
+# are times where the the test fails nearing the end of the solution. Since the algorithm is depth first and was unable
+# to cut out paths
+# or prune the tree with invalid digits it will attempt to continue finding values. The algorithm is also worse when
+# the number of single digit and double digits are closer in value, this results in the most possible combinations.
 
 
 
@@ -76,6 +81,7 @@ class TestLottery(unittest.TestCase):
         self.assertTrue(Lottery.is_lottery('12345678'))
         self.assertTrue(Lottery.is_lottery('123456789'))
         self.assertTrue(Lottery.is_lottery('12345678911'))
+        self.assertTrue(Lottery.is_lottery('938592884754'))
         self.assertTrue(Lottery.is_lottery('4938532894754'))
 
 
@@ -101,20 +107,21 @@ class Lottery:
     @staticmethod
     def create_digit(single, double, num_set, str):
 
-        # print "single: {} double: {} arr: {} str: {}".format(single, double, num_set, str)
+        print "single: {} double: {} arr: {} str: {}".format(single, double, num_set, str)
         is_lottery = False
         if len(str) == 0 and single == 0 and double == 0:
-            # print num_set
+            print num_set
             is_lottery = True
         else:
-            new_set = num_set.copy()
             if double > 0:
+                new_set = num_set.copy()
                 if str[0] != '0':
                     temp_num = int(str[0:2])
                     if temp_num > 0 and temp_num <= MAX_NUM and temp_num not in new_set:
                         new_set.add(temp_num)
                         is_lottery = is_lottery or Lottery.create_digit(single, double-1, new_set, str[2:])
-            if single > 0:
+            if single > 0 and is_lottery is False:
+                new_set = num_set.copy()
                 temp_num = int(str[0])
                 if temp_num > 0 and temp_num <= MAX_NUM and temp_num not in new_set:
                     new_set.add(temp_num)
@@ -131,5 +138,5 @@ def main(argv):
 
 
 if __name__ == "__main__":
-    # unittest.main()
+    unittest.main()
     main(sys.argv)
